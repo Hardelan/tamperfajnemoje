7e075493d450ca46362b9aa9058aaff82e3301e1
Hardelan <rtitanis@gmail.com>
2025-08-31 18:41:22 +0200

    Update temperfajnemoje.user.js

--- temperfajnemoje.user.js
+++ temperfajnemoje.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         KeyDropBot - wersja połączona finalna + kliknięcia zamiast usuwania (optymalizacja CPU)
 // @namespace    https://key-drop.com
-// @version      6.4
+// @version      6.5
 // @description  Giveaway + UI cleanup + CPU optymalizacja + kliknięcia zamiast usuwania wybranych elementów
 // @match        https://key-drop.com/pl/giveaways*
 // @match        https://key-drop.com/pl/giveaways/list
@@ -110,7 +110,7 @@
             await new Promise(r => setTimeout(r, 1000));
         }
 
-        if (!price || price >= 270) window.location.replace("https://key-drop.com/pl/giveaways/list/");; 
+        if (!price || price >= 268) time.sleep(20000); window.location.replace("https://key-drop.com/pl/giveaways/list/");; 
 
         if (price >= 1) {
             const subStart = Date.now();
